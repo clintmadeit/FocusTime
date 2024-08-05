19b65909791f5edd1fced29ddfb52f29fc866c81
clintmadeit <clintonbrirdley@gmail.com>
2024-08-05 17:56:15 +0300

    implementing subject statuses tracking

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
-import React, {useState} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Platform, StyleSheet, View} from 'react-native';
 import {Focus} from './src/features/focus/Focus';
 import {colors} from './src/utils/colors';
 import {Timer} from './src/features/timer/Timer';
 import {spacing} from './src/utils/spacing';
 
+const STATUSES = {
+  COMPLETE: 1,
+  CANCELLED: 2,
+};
+
 function App() {
   const [focusSubject, setFocusSubject] = useState('Reading');
+  const [focusHistory, setFocusHistory] = useState([]);
+
+  const addFocusHistorySubjectWithState = (subject, status) => {
+    setFocusHistory([...focusHistory, {subject, status}]);
+  };
+
+  useEffect(() => {
+    if (focusSubject) {
+      setFocusHistory([...focusHistory, focusSubject]);
+    }
+  }, [focusSubject]);
+
+  console.log(focusHistory);
+
   return (
     <View style={styles.container}>
       {focusSubject ? (
         <Timer
           focusSubject={focusSubject}
           onTimerEnd={() => {
+            addFocusHistorySubjectWithState(focusSubject, STATUSES.COMPLETE);
             setFocusSubject(null);
           }}
           clearSubject={() => {
+            addFocusHistorySubjectWithState(focusSubject, STATUSES.CANCELLED);
             setFocusSubject(null);
           }}
         />
